d5ad078587f93b0483b3fc4276f226cd83918a54
ambersanti <carolinasandoval879@gmail.com>
2024-03-14 21:30:29 -0600

    Changed the form size

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,25 @@
-import './App.css';
-import Header from './components/Header'
-import Home from './components/Home'
-import AboutMe from './components/AboutMe'
-import Resume from './components/Resume'
-import Portfolio from './components/Portfolio'
-import Contact from './components/Contact'
-import Footer from './components/Footer'
+import "./App.css";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import AboutMe from "./components/AboutMe";
+import Resume from "./components/Resume";
+import Portfolio from "./components/Portfolio";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "react-toastify/dist/ReactToastify.css";
+// import './normalize.css'
 
 function App() {
   return (
-    <div >
-      {/* <Header/> */}
-      <Home/>
-      <AboutMe/>
-      <Portfolio/>
-      <Resume/>
-      <Contact/>
-      <Footer/>
-
+    <div>
+      <Header/>
+      <Home />
+      <AboutMe />
+      <Portfolio />
+      <Resume />
+      <Contact />
+      <Footer />
     </div>
   );
 }
