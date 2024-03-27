3fbc8261b64c6921690e1079c398e9bcc47361b6
ambersanti <carolinasandoval879@gmail.com>
2024-03-26 18:15:52 -0600

    added public folder in server

--- server/index.js
+++ server/index.js
@@ -2,10 +2,11 @@ const express = require("express");
 const cors = require("cors");
 const contactRoutes = require("./routes/contact");
 const CV = require('.')
+const path = require('path')
 
 const app = express();
-
 app.use(express.json());
+app.use(express.static(path.join(__dirname + "/public")));
 app.use(
   cors({
     origin: [
@@ -15,7 +16,7 @@ app.use(
   })
 );
 app.use("/api", contactRoutes);
-app
+
 const port = process.env.PORT ||5000;
 
 app.listen(port, () => {
