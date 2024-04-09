8d2a08b2cd7dc93950c8ab95daee28ec52da24b9
ambersanti <carolinasandoval879@gmail.com>
2024-04-09 13:07:20 -0600

    Deleted amberly.dev in CORS

--- server/index.js
+++ server/index.js
@@ -10,10 +10,8 @@ app.use(express.static(path.join(__dirname + "/public")));
 app.use(
   cors({
     origin: [
-      "http://localhost:3000",
-      "https://www.amberly.dev",
-      "https://https://carolina-portfolio-3706c48e8bfe.herokuapp.com",
-    ],
+    "http://localhost:3000",
+    "https://carolina-portfolio-3706c48e8bfe.herokuapp.com"],
     credentials: true,
   })
 );
